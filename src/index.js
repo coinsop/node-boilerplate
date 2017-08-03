4b4c68276eeb474845a9d0b17a4b53b147015fff
Luis Fuenmayor <fuelusumar@gmail.com>
2017-08-02 22:43:11 -0400

    added new key for each txn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const blockHlpr = require('./helpers/block');
 const proofHlpr = require('./helpers/proof');
 const asymCryptoHlpr = require('./helpers/asymCrypto');
 
-const blockchain = new BlockChain('000');
+const blockchain = new BlockChain('0000');
 
 let prevBlock = blockchain.getLastBlock();
 winston.log('info', '----------------------------------------------------------------------');
@@ -16,14 +16,13 @@ winston.log('info', `Nonce: ${prevBlock.__nonce}`);
 winston.log('info', `Txns: ${prevBlock.__data.length}`);
 winston.log('info', '----------------------------------------------------------------------');
 const numOfBlocksToAdd = 32;
-const numOfTxnsInData = 32;
-
+const numOfTxnsInData = 1;
 for (let i = 0; i < numOfBlocksToAdd; i += 1) {
-  const pair = keypair();
-  const privateKey = pair.private;
-  const publicKey = pair.public;
   const dataBlock = [];
   for (let j = 0; j < numOfTxnsInData; j += 1) {
+    const pair = keypair();
+    const privateKey = pair.private;
+    const publicKey = pair.public;
     const txn = new Txn('alice', 'bob', 0.00001, { hola: 'mundo' }, publicKey, privateKey);
     if (asymCryptoHlpr.verify(txn.__publicKey, txn.__signature, txn.toMessage())) {
       dataBlock.push(txn);
